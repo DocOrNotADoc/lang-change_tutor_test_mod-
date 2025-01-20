8b2c66045b5789162eb562876de046a35e56ec0b
Vik <leesunsinn@protonmail.com>
2025-01-20 18:29:52 +0900

    switch works

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,22 @@
 const langButtons = document.querySelectorAll("[data-btn]");
 const allLangs = ["en", "ru"];
-const currentPathName = window.location.pathname;
 
-let currentLang = localStorage.getItem("language") || checkBrowserLang() || "en";
+let userLang = localStorage.getItem("language") || checkBrowserLang();
 let currentTexts = {};
 
-const homeTexts = {
+// Проверка языка браузера
+function checkBrowserLang() {
+	const navLang = navigator.language.slice(0, 2).toLowerCase();
+	const result = allLangs.some((elem) => {
+		return elem === navLang;
+	});
+	if (result) {
+		return navLang;
+	}
+}
+
+//текты
+const translatedTexts = {
 	"home_page-title": {
 		en: "Homepage",
 		ru: "Домашняя страница",
@@ -22,20 +33,16 @@ const homeTexts = {
 		en: "Third paragraph",
 		ru: "Третий параграф",
 	},
-	"home_page-4": {
-		en: "Another page",
-		ru: "Другая страница",
-	},
 };
-
-currentTexts = homeTexts;
+currentTexts = translatedTexts;
 
 // Изменение языка у текстов
 function changeLang() {
 	for (const key in currentTexts) {
 		let elem = document.querySelector(`[data-lang=${key}]`);
+
 		if (elem) {
-			elem.textContent = currentTexts[key][currentLang];
+			elem.textContent = currentTexts[key][userLang];
 		}
 	}
 }
@@ -44,69 +51,32 @@ changeLang();
 // Вешаем обработчики на каждую кнопку
 langButtons.forEach((btn) => {
 	btn.addEventListener("click", (event) => {
-		if (!event.target.classList.contains("header__btn_active")) {
-			currentLang = event.target.dataset.btn;
-			localStorage.setItem("language", event.target.dataset.btn);
-			resetActiveClass(langButtons, "header__btn_active");
-			btn.classList.add("header__btn_active");
-			changeLang();
-		}
+		userLang = event.target.dataset.btn;
+		localStorage.setItem("language", event.target.dataset.btn);
+		// console.log(event.target.dataset.btn);
+		changeLang();
 	});
 });
 
-// Сброс активного класса у переданного массива элементов
-function resetActiveClass(arr, activeClass) {
-	arr.forEach((elem) => {
-		elem.classList.remove(activeClass);
-	});
-}
 
-// Проверка активной кнопки
-function checkActiveLangButton() {
-	switch (currentLang) {
-		case "en":
-			document
-				.querySelector('[data-btn="en"]')
-				.classList.add("header__btn_active");
-			break;
-		case "ru":
-			document
-				.querySelector('[data-btn="ru"]')
-				.classList.add("header__btn_active");
-			break;
-		default:
-			document
-				.querySelector('[data-btn="en"]')
-				.classList.add("header__btn_active");
-			break;
-	}
-}
-checkActiveLangButton();
-
-// Проверка языка браузера
-function checkBrowserLang() {
-	const navLang = navigator.language.slice(0, 2).toLowerCase();
-	const result = allLangs.some((elem) => {
-		return elem === navLang;
-	});
-	if (result) {
-		return navLang;
-	}
-}
-
-// console.log("navigator.language", checkBrowserLang());
-// console.log("currentPathName:", currentPathName);
-
-// dropdown функция
+// Dropdown список для выбора языка
 function setLanguage(language, flagSrc) {
 	const button = document.getElementById('language-button');
 	button.innerHTML = `<img src="${flagSrc}" alt="${language}" class="flag">${language}`;
+	
 }
 
-// ставим английский язык по умолчанию
+// ставим язык по умолчанию
 window.onload = function() {
-	setLanguage('EN', 'icons/EN-flag-icon.svg');
+	switch (userLang) {
+		case 'ru':
+			setLanguage('ru', 'icons/ru-flag-icon.svg');
+			break;
+		case 'en':
+			setLanguage('en', 'icons/en-flag-icon.svg');
+		break;
+		default:
+			setLanguage('en', 'icons/en-flag-icon.svg');
+			break;
+	}
 };
-
-
-
